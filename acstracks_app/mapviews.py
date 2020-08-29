725d69f56eb56a763eb61009478396bb101f3e35
AadSchippers <aadschippers@gmail.com>
2020-08-29 14:40:41 +0200

    fixes

--- acstracks_app/mapviews.py
+++ acstracks_app/mapviews.py
@@ -24,7 +24,27 @@ def process_gpx_file(filename):
     ave_lon = sum(p[1] for p in points)/len(points)
 
     # Load map centred on average coordinates
-    my_map = folium.Map(location=[ave_lat, ave_lon], zoom_start=14)
+    my_map = folium.Map(location=[ave_lat, ave_lon], zoom_start=12)
+
+    min_lat = float(9999999)
+    max_lat = float(-9999999)
+    min_lon = float(9999999)
+    max_lon = float(-9999999)
+    for p in points:
+        if min_lat > p[0]:
+            min_lat = p[0]
+        if max_lat < p[0]:
+            max_lat = p[0]
+        if min_lon > p[1]:
+            min_lon = p[1]
+        if max_lon < p[1]:
+            max_lon = p[1]
+
+    sw = tuple([min_lat, min_lon])
+    ne = tuple([max_lat, max_lon])
+
+    my_map.fit_bounds([sw, ne]) 
+
 
     # add a markers
     # for each in points:  
@@ -34,7 +54,7 @@ def process_gpx_file(filename):
     # nice green circle
     folium.vector_layers.CircleMarker(
             location=[points[0][0], points[0][1]],
-            radius=9,
+            radius=12,
             color="white",
             weight=1,
             fill_color="green",
@@ -48,14 +68,14 @@ def process_gpx_file(filename):
             fill_opacity=1,
             color="white",
             number_of_sides=3,
-            radius=3,
+            radius=4,
             rotation=0,
         ).add_to(my_map)
 
     # nice red circle
     folium.vector_layers.CircleMarker(
             location=[points[-1][0], points[-1][1]],
-            radius=9,
+            radius=12,
             color="white",
             weight=1,
             fill_color="red",
@@ -69,7 +89,7 @@ def process_gpx_file(filename):
             fill_opacity=1,
             color="white",
             number_of_sides=4,
-            radius=3,
+            radius=4,
             rotation=45,
             popup="popup"
         ).add_to(my_map)
