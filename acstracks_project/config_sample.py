4fa0e3d10b34fbca67bca34d9fed3c0893497335
Aad Schippers <aadschippers@gmail.com>
2021-11-11 16:01:08 +0100

    minor update

--- acstracks_project/config_sample.py
+++ acstracks_project/config_sample.py
@@ -3,12 +3,13 @@
 # SECURITY WARNING: keep the secret key used in production secret!
 # A secret key is neccessary to run this site.
 # You can generate your SECRET_KEY here: https://djecrety.ir/
-SECRET_KEY = ''
+SECRET_KEY = 'yj2mk236^11#f%sgbst#^c+2njhj^6l7bz8azw^e1=7%na3vo='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-# Replace 'yoursite.com" with your site url:
+# Add your site url to the list:
 ALLOWED_HOSTS = [
-    'yoursite.com',
+    'localhost',
+    '127.0.0.1',
     ]
